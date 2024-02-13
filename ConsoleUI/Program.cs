6a07b9eec4bd94fc35e20c2f62ca25d1b789504c
Computing2025 <ozk8000@gmail.com>
2024-02-14 01:39:51 +0300

    Join özelliği eklendi.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -5,24 +5,28 @@ namespace ConsoleUI
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            // ProductTest();
+             ProductTest();
+            //CategoryTest();
+        }
+
+        private static void CategoryTest()
+        {
             CategoryManager categoryManager = new(new EfCategoryDal());
             foreach (var category in categoryManager.GetAll())
             {
                 Console.WriteLine(category.CategoryName);
             }
-
         }
 
         private static void ProductTest()
         {
             ProductManager productManager = new(new EfProductDal());
 
-            foreach (var product in productManager.GetAllByCategoryId(2))
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName +" - "+ product.CategoryName);
             }
         }
     }
