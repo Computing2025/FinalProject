ca4a321146c09dd3f6872372b29b9c9244f5d700
Computing2025 <ozk8000@gmail.com>
2024-02-18 19:56:12 +0300

    SuccessDataResult ve ErrorDataResult eklendi.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static readonly string ProductAdded = "Ürün eklendi.";
+        public static readonly string ProductNameInvalid = "Ürün ismi geçersiz.";
+        public static readonly string MaintenanceTime = "Sistem bakımda.";
+        public static readonly string ProductsListed = "Ürünler listelendi.";
+    }
+}
+
+
